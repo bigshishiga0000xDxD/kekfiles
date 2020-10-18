388abc4c052b4ca7e079a94bb11daab4313492b2
danil <danil.sheshe@gmail.com>
2020-10-18 17:14:40 +0300

    update setup && more geoma

--- competitive_programming/lib/geoma.h
+++ competitive_programming/lib/geoma.h
@@ -1,20 +1,41 @@
+const long double pi = acos(-1);
+const long double eps = 5e-8;
+
+template <typename T>
+inline int sign(T x) {
+    if (abs(x) < eps) {
+        return 0;
+    }
+    else if (x > 0) {
+        return 1;
+    }
+    else {
+        return -1;
+    }
+}
+
 template <typename T>
 struct Point {
     T x, y;
  
     Point() {}
     Point(T _x, T _y) : x(_x), y(_y) {}
+
+    template <typename V>
+    operator Point<V>() {
+        return Point <V> (static_cast <V> (x), static_cast <V> (y));
+    }
  
     bool operator==(const Point <T>& s) {
-        return x == s.x && y == s.y;
+        return abs(x - s.x) < eps && abs(y - s.y) < eps;
     }
 
     bool operator!=(const Point <T>& s) {
-        return x != s.x || y != s.y;
+        return abs(x - s.x) > eps || abs(y - s.y) > eps;
     }
 
     bool operator<(const Point <T>& s) {
-        if (x != s.x) {
+        if (abs(x - s.x) < eps) {
             return x < s.x;
         }
         else {
@@ -23,7 +44,7 @@ struct Point {
     }
  
     bool operator>(const Point <T>& s) {
-        if (x != s.x) {
+        if (abs(x - s.x) < eps) {
             return x > s.x;
         }
         else {
@@ -36,8 +57,8 @@ struct Point {
     }
  
     Point& operator+=(const Point <T>& s) {
-        this->x += s.x;
-        this->y += x.y;
+        x += s.x;
+        y += s.y;
         return *this;
     }
  
@@ -45,9 +66,14 @@ struct Point {
         return "{" + to_string(x) + ", " + to_string(y) + "}";
     }
 };
- 
+
 template <typename T>
-long double distance(Point <T> a, Point <T> b) {
+inline bool areSame(Point <T> a, Point <T> b) {
+    return abs(a.x - b.x) < eps && abs(a.y - b.y) < eps;
+}
+
+template <typename T>
+inline long double distance(Point <T> a, Point <T> b) {
     return sqrt(static_cast <long double> 
         ((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y)));
 }
@@ -69,22 +95,60 @@ struct Vector {
     T x, y;
  
     Vector() {};
-    Vector(Point <T> a, Point <T> b) : x(b.x - a.x), y(b.y - a.y) {}
+
+    template <typename V>
+    Vector(Point <V> a, Point <V> b) : x(b.x - a.x), y(b.y - a.y) {}
+
     Vector(T a, T b) : x(a), y(b) {}
  
-    ld len() {
+    inline long double len() const {
         return sqrt(x * x + y * y);
     }
- 
+
+    inline void normalize() {
+        auto l = len();
+        x /= l, y /= l;
+    }
+
+    Vector <T> operator-() {
+        return { -x, -y };
+    }
+
     Vector <T> operator+(Vector <T> v) {
         return { x + v.x, y + v.y };
+    }
+
+    Vector <T>& operator+=(Vector <T> v) {
+        x += v.x, y += v.y;
+        return *this;
+    }
+
+    Vector <T> operator-(Vector <T> v) {
+        return { x - v.x, y - v.y };
+    }
+
+    Vector <T>& operator-=(Vector <T> v) {
+        x -= v.x, y -= v.y;
+        return *this;
+    }
+
+    template <typename V>
+    Vector <T> operator*(V e) {
+        return { x * e, y * e };
+    }
+
+    template <typename V>
+    Vector <T>& operator*=(V e) {
+        x *= e, y *= e;
+        return *this;
     }
  
     operator string() {
         return "{" + to_string(x) + ", " + to_string(y) + "}";
     }
 };
- 
+
+
 template <typename T>
 Point <T> operator+(const Point <T>& p, const Vector <T>& v) {
     return { p.x + v.x, p.y + v.y };
@@ -109,15 +173,20 @@ ostream& operator<< (ostream& out, Vector <T>& v) {
  
  
 template <typename T>
-T scal(Vector <T> a, Vector <T> b) {
+inline T scal(Vector <T> a, Vector <T> b) {
     return a.x * b.x + a.y * b.y;
 }
  
 template <typename T>
-T pscal(Vector <T> a, Vector <T> b) {
+inline T pscal(Vector <T> a, Vector <T> b) {
     return a.x * b.y - a.y * b.x;
 }
- 
+
+template <typename T>
+inline long double degree(Vector <T> a, Vector <T> b) {
+    return atan2(pscal <T>(a, b), scal <T>(a, b));
+}
+
 template <typename T>
 struct Line {
     T a, b, c;
@@ -137,16 +206,21 @@ struct Line {
     }
 
     template <typename V>
-    bool belongs(Point <V> p) {
+    inline T eval(const Point <V>& p) const {
+        return a * p.x + b * p.y + c;
+    }
+
+    template <typename V>
+    inline bool belongs(const Point <V>& p) const {
         return abs(a * p.x + b * p.y + c) < eps;
     }
  
-    void normalize() {
+    inline void normalize() {
         T t = sqrt(a * a + b * b);
         a /= t, b /= t, c /= t;
     }
  
-    T getY(T x) {
+    inline T getY(const T x) const {
         if (b) {
             return (a * x + c) / (-b);
         }
@@ -154,7 +228,7 @@ struct Line {
             return 0;
         }
     }
-    T getX(T y) {
+    inline T getX(const T y) const {
         if (a) {
             return (b * y + c) / (-a);
         }
@@ -162,37 +236,61 @@ struct Line {
             return 0;
         }
     }
+
+    inline Point <T> getPoint() const {
+        return {
+            - a * c / (a * a + b * b),
+            - b * c / (a * a + b * b)
+        };
+    }
 };
  
 template <typename T>
 vector <Point <T>> intersect(Line <T>& a, Line <T>& b) {
-    T t = abs(a.a * b.b - b.a * a.b); 
-    if (t < eps) {
+    T t = a.a * b.b - b.a * a.b; 
+    if (abs(t) < eps) {
         return { };
     }
     else {
-        T y = (b.a * a.c - a.a * b.c) / t;
-        T x = (-a.b * y - a.c) / a.a;
-        return { { x, y } };
+        return {{
+            (b.c * a.b - a.c * b.b) / t,
+            (b.a * a.c - a.a * b.c) / t
+        }};
+    }
+}
+
+template <typename T>
+Point <T> findProjection(Point <T> p, Line <T> l) {
+    T dist = l.eval(p) / sqrt(l.a * l.a + l.b * l.b);
+
+    Vector <T> v = { l.a, l.b };
+    v.normalize();
+    v *= dist;
+
+    if (l.belongs(p + v)) {
+        return p + v;
+    }
+    else {
+        return p - v;
     }
 }
  
 template <typename T>
-bool OnBeam(Point <T> P1, Point <T> P2, // beam
-    Point <T> P /* point */) {
-    if (pscal <T>({ P1, P }, { P1, P2 }) != 0) {
+bool onBeam(Point <T> P1, Point <T> P2, Point <T> P) {
+    if (abs(pscal <T>({ P1, P }, { P1, P2 })) > eps) {
         return false;
     }
-    if (scal <T>({ P1, P }, { P2, P }) <= 0 || scal <T>({ P1, P2 }, { P2, P }) >= 0) {
+    if (scal <T>({ P1, P }, { P2, P }) - eps <= 0 ||
+        scal <T>({ P1, P2 }, { P2, P }) + eps >= 0) {
         return true;
     }
     return false;
 }
  
 template <typename T>
-bool IntoAngle(Point <T> a, Point <T> o, Point <T> b, // angle
-    Point <T> p /* point */) {
-    if (OnBeam(o, a, p) || OnBeam(o, b, p)) {
+bool insideAngle(Point <T> a, Point <T> o, Point <T> b, Point <T> p) {
+    // fix for real numbers
+    if (onBeam(o, a, p) || onBeam(o, b, p)) {
         return true;
     }
  
@@ -206,8 +304,9 @@ bool IntoAngle(Point <T> a, Point <T> o, Point <T> b, // angle
     return false;
 }
 
+
 template <typename T>
-vector <Point <T>> _ConvexHull(typename vector <Point <T>> :: iterator __first,
+vector <Point <T>> _convexHull(typename vector <Point <T>> :: iterator __first,
        typename vector <Point <T>> :: iterator __last) {
     vector <Point <T>> res;
 
@@ -229,13 +328,13 @@ vector <Point <T>> _ConvexHull(typename vector <Point <T>> :: iterator __first,
 }
 
 template <typename T>
-Point <T> RadialSort(typename vector <Point <T>> :: iterator __first, 
+Point <T> radialSort(typename vector <Point <T>> :: iterator __first, 
         typename vector <Point <T>> :: iterator __last) {
-    Point m = *min_element(__first, __last);
+    Point <T> m = *min_element(__first, __last);
 
     sort(__first, __last, [m](Point <T> x, Point <T> y) {
         Vector <T> f = { m, x }, s = { m, y };
-        if (pscal<T>(f, s) == 0) {
+        if (abs(pscal<T>(f, s)) < eps) {
             return f.len() < s.len();
         }
         return pscal<T>(f, s) > 0;
@@ -245,9 +344,275 @@ Point <T> RadialSort(typename vector <Point <T>> :: iterator __first,
 }
 
 template <typename T>
-vector <Point <T>> ConvexHull(typename vector <Point <T>> :: iterator __first,
+vector <Point <T>> convexHull(typename vector <Point <T>> :: iterator __first,
         typename vector <Point <T>> :: iterator __last) {
-    RadialSort<T>(__first, __last);
-    return _ConvexHull<T>(__first, __last);
+    radialSort<T>(__first, __last);
+    return _convexHull<T>(__first, __last);
 }
+
+template <typename T>
+struct Circle {
+    Point <T> cent;
+    long double r;
+
+    Circle <T>() {}
+    Circle <T>(Point <T>& _cent, long double _r) : cent(_cent), r(_r) {}
+    Circle <T>(T x, T y, long double _r) : cent(x, y), r(_r) {}
+
+    template <typename V>
+    inline bool belongs(Point <V> p) const {
+        return abs((p.x - cent.x) * (p.x - cent.x) +
+               (p.y - cent.y) * (p.y - cent.y) - r * r) < eps;
+    }
+
+    inline long double length() const {
+        return 2 * pi * r;
+    }
+
+    inline long double area() const {
+        return pi * r * r;
+    }
+};
+
+template <typename T>
+vector <Point <T>> intersect(Circle <T> c, Line <T> l) {
+    Point <T> p = findProjection(c.cent, l);
+    long double dist = distance(c.cent, p);
+
+    if (abs(dist - c.r) < eps) {
+        return { p };
+    }
+    else if (dist > c.r) {
+        return { };
+    }
+    else {
+        Vector <T> v = { -l.b, l.a };
+        v.normalize();
+        v *= sqrt(c.r * c.r - dist * dist);
+
+        return { p - v, p + v };
+    }
+}
+
+template <typename T>
+vector <Point <T>> intersect(Circle <T> x, Circle <T> y) {
+    if (areSame(x.cent, y.cent)) {
+        return { };
+    }
+    else {
+        auto a = x.cent, b = y.cent;
+
+        Line <T> l;
+        l.a = 2 * (a.x - b.x);
+        l.b = 2 * (a.y - b.y);
+        l.c = b.x * b.x + b.y * b.y - y.r * y.r - a.x * a.x - a.y * a.y + x.r * x.r;
+
+        return intersect(x, l);
+    } 
+}
+
+template <typename T>
+vector <Point <T>> tangent(Circle <T> c, Point <T> p) {
+    if (distance(c.cent, p) < c.r) {
+        return { };
+    }
+    else if (c.belongs(p)) {
+        return { p };
+    }
+    else {
+        long double dist = distance(p, c.cent);
+        Circle <T> d(p, sqrt(dist * dist - c.r * c.r));
+        return intersect(c, d);
+    }
+}
+
+template <typename T>
+bool onSegment(Point <T> s, Point <T> f, Point <T> p) {
+    return (Line <long double>(s, f).belongs(p) &&
+        p.x + eps >= min(s.x, f.x) && p.x - eps <= max(s.x, f.x) &&
+        p.y + eps >= min(s.y, f.y) && p.y - eps <= max(s.y, f.y)) ||
+        areSame(s, p) || areSame(f, p);
+}
+
+template <typename T>
+bool insideTriangle(Point <T> p, Point <T> a, Point <T> b, Point <T> c) {
+    T s = 0;
+    s += abs(pscal <T>({ p, a }, { p, b }));
+    s += abs(pscal <T>({ p, b }, { p, c }));
+    s += abs(pscal <T>({ p, a }, { p, c }));
+
+    T x = abs(pscal <T>({ a, b }, { a, c }));
+
+    return abs(s - x) < eps;
+}
+
+template <typename T>
+void precalc(vector <Point <T>>& a) {
+    rotate(a.begin(), min_element(a.begin(), a.end()), a.end());
+}
+
+/**
+ * logn, precalc needed
+*/
+template <typename T>
+bool insidePolygon(vector <Point <T>>& a, Point <T> p) {
+    if (p < a.front()) {
+        return false;
+    }
+
+    int l = 1, r = sz(a) - 1, m;
+    while (r - l > 1) {
+        m = (l + r) / 2;
+
+        if (pscal <T>({ a.front(), a[m] }, { a.front(), p }) > 0) {
+            l = m;
+        }
+        else {
+            r = m;
+        }
+    }
+
+    return insideTriangle(p, a.front(), a[l], a[r]);
+}
+
+template <typename T>
+bool onCircle(Point <T> a, Point <T> b, Point <T> c, Point <T> d) {
+    auto degreeEq = [=] (Point <T> a, Point <T> b, Point <T> c,
+            Point <T> x, Point <T> y, Point <T> z) {
+
+        return abs(pscal<T> ({ b, a }, { b, c }) * scal<T> ({ y, x }, { y, z }) -
+                scal<T> ({ b, a }, { b, c }) * pscal<T> ({ y, x }, { y, z })) < eps;
+    };
+
+    return abs(pscal<T> ({ a, b }, { b, c })) > eps &&
+           abs(pscal<T> ({ a, c }, { c, d })) > eps &&
+           abs(pscal<T> ({ b, c }, { c, d })) > eps &&
+           (degreeEq(a, b, d, a, c, d) || degreeEq(a, b, c, a, d, c));
+}
+
+template <typename T>
+long double pointToSegment(Point <T> p, Point <T> a, Point <T> b) {
+    long double ans = min(distance(p, a), distance(p, b));
+    long double s = abs(pscal <T> ({ p, a }, { p, b }));
+    if (scal <T>({ a, b }, { b, p }) <= 0 && scal <T>({ b, a }, { a, p }) <= 0) {
+        ans = min(ans, s / distance(a, b));
+    }
+    return ans;
+}
+
+template <typename T>
+long double pointToBeam(Point <T> p, Point <T> a, Point <T> b) {
+    long double ans = distance(p, a);
+    long double s = abs(pscal <T> ({ p, a }, { p, b }));
+    if (scal <T>({ b, a }, { a, p }) <= 0) {
+        ans = min(ans, s / distance(a, b));
+    }
+    return ans;
+}
+
+template <typename T, typename V>
+long double pointToLine(Point <T> p, Line <V> l) {
+    return abs(l.eval(p) / sqrt(l.a * l.a + l.b * l.b));
+}
+
+template <typename T>
+long double segmentToSegment(Point <T> a, Point <T> b, Point <T> c, Point <T> d) {
+    Line <long double> x(a, b), y(c, d);
+    auto p = intersect(x, y);
+
+    if (!p.empty() &&
+        onSegment(static_cast<Point<long double>>(a),
+                  static_cast<Point<long double>>(b),
+                  p.front()) &&
+        onSegment(static_cast<Point<long double>>(c),
+                  static_cast<Point<long double>>(d),
+                  p.front())) {
+        return 0;
+    }
+    else {
+        return min({ pointToSegment(a, c, d), pointToSegment(b, c, d),
+            pointToSegment(c, a, b), pointToSegment(d, a, b) });
+    }
+}
+
+template <typename T>
+long double segmentToBeam(Point <T> a, Point <T> b, Point <T> c, Point <T> d) {
+    Line <long double> x(a, b), y(c, d);
+    auto p = intersect(x, y);
+
+    if (!p.empty() &&
+        onSegment(static_cast<Point<long double>>(a),
+                  static_cast<Point<long double>>(b),
+                  p.front()) &&
+        onBeam(static_cast<Point<long double>>(c),
+               static_cast<Point<long double>>(d),
+               p.front())) {
+        return 0;
+    }
+    else {
+        return min({ pointToBeam(a, c, d), pointToBeam(b, c, d), pointToSegment(c, a, b) });
+    }
+}
+
+template <typename T, typename V>
+long double segmentToLine(Point <T> a, Point <T> b, Line <V> l) {
+    Line <long double> x(a, b);
+    auto p = intersect(x, l);
+
+    if (!p.empty() &&
+        onSegment(static_cast<Point<long double>>(a),
+                  static_cast<Point<long double>>(b),
+                  p.front())) {
+        return 0;
+    }
+
+    return min({ pointToLine(a, l), pointToLine(b, l) });
+}
+
+template <typename T>
+long double beamToBeam(Point <T> a, Point <T> b, Point <T> c, Point <T> d) {
+    Line <long double> l1(a, b), l2(c, d);
+    auto v = intersect(l1, l2);
+
+    auto A = static_cast<Point<long double>>(a);
+    auto B = static_cast<Point<long double>>(b);
+    auto C = static_cast<Point<long double>>(c);
+    auto D = static_cast<Point<long double>>(d);
+
+    if (!v.empty() && onBeam(A, B, v.front()) && onBeam(C, D, v.front())) {
+        return 0;
+    }
+    else {
+        return min(pointToBeam(a, c, d), pointToBeam(c, a, b));
+    }
+}
+
+template <typename T, typename V>
+long double beamToLine(Point <T> a, Point <T> b, Line <V> l) {
+    Line <V> l2(a, b);
+    auto v = intersect(l, l2);
+
+    auto A = static_cast<Point<long double>>(a);
+    auto B = static_cast<Point<long double>>(b);
+
+    if (!v.empty() && onBeam(A, B, v.front())) {
+        return 0;
+    }
+    else {
+        return pointToLine(a, l);
+    }
+}
+
+template <typename T>
+long double lineToLine(Line <T> a, Line <T> b) {
+    auto v = intersect(a, b);
+
+    if (v.empty()) {
+        return pointToLine(a.getPoint(), b);        
+    }
+    else {
+        return 0;
+    }
+}
+
 
--- competitive_programming/setup/main.cpp
+++ competitive_programming/setup/main.cpp
@@ -1,32 +1,37 @@
-﻿#ifdef LOCAL
+﻿//#pragma GCC optimize("Ofast,unroll-loops")
+ 
+#ifdef LOCAL
 #include "debug.h"
 #else
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 #define debug(x...)
 #endif
-
-//#define int ll
-//#pragma GCC optimize("Ofast")
-
+ 
 using namespace std;
+ 
+//#define int ll
+ 
 typedef long long ll;
 typedef long double ld;
 typedef pair <int, int> pii;
 #define sz(x) int((x).size())
-
+ 
+template <typename T>
+using ordered_set = __gnu_pbds::tree <T, __gnu_pbds::null_type, less<T>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update>;
+ 
 #ifdef ONLINE_JUDGE
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 #else
 mt19937 rng(1000 - 7);
 #endif
-
+ 
 const int N = 1e5 + 10;
-const int M = 1e3 + 10;
-const int MOD = 998244353;
-//const int MOD = 1e9 + 7;
-const ld eps = 1e-6;
+const ll MOD = 1e9 + 7;
+//const ll MOD = 998244353;
+const ld eps = 5e-8;
 const pii dir[] = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
-
 
 
 signed main() {
